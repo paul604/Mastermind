8b216a8277dd63ebebbd772d8d6c2e31781d6e86
paul <paul604@users.noreply.github.com>
2016-11-25 10:16:45 +0100

    Update controleurJeu.php

--- controleur/controleurJeu.php
+++ controleur/controleurJeu.php
@@ -14,17 +14,40 @@ class ControleurJeu{
 	}
 
 	public function jeu($tab){
+
 		if($tab==null){
 			$plateau=$this->modeleJeu->creeJeu();
 		}else{
 			$plateau=$this->modeleJeu->Jeu($tab);
 		}
+
 		if(gettype($plateau)== "boolean"){
 			echo "vue victoire";
 		}else{
-			$this->vueJeu->Jeu($plateau);
+			$plateau2=array(
+				array(
+				),//1
+				array(
+				),//2
+				$plateau[2],
+			);
+
+			//transform en string
+			for ($i=0; $i <=1; $i++) {
+				$j=0;
+				foreach ($plateau[$i] as $tab) {
+					$plateau2[$i][$j]=array();
+					foreach ($tab->get() as $val){
+						$plateau2[$i][$j][]=$val;
+					}
+					$j++;
+				}
+			}
+
+			$this->vueJeu->Jeu($plateau2);
 		}
-	}
+
+	}	
 
 }
 ?>
