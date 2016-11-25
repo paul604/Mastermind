2576b4519658c1183c268a79d242af5a504a4064
paul <paul604@users.noreply.github.com>
2016-11-25 10:49:55 +0100

    Update script.js

--- vue/script.js
+++ vue/script.js
@@ -1,1 +1,9 @@
-
+var tab=[0,0,0,0];
+function set(color) {
+        for (i = 0; i < tab.length; i++) {
+            if(tab[i]==0){
+              tab[i]=color;
+              document.getElementsByClassName('courant')[i].id="c"+color;
+            }
+        }
+}
