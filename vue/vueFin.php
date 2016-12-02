db7623dcfff288f18624d0e1ad80d45ced768cd9
paul <paul604@users.noreply.github.com>
2016-12-02 14:31:04 +0100

    Update vueFin.php

--- vue/vueFin.php
+++ vue/vueFin.php
@@ -18,10 +18,11 @@ class VueFin{
 		<body>
 			<div id="solusJeuChoix">
 				<div id="name">
-					<h1>Mastermind</h1>
+					<h1>Mastermind <?php  echo $plateau[2] ?>/10</h1>
 				</div>
 				<div id="solution">
 					<ul id="tabSolus">
+						<li id=indice></li>
 						<li class="c<?php echo $solusion[0]?>"></li>
 						<li class="c<?php echo $solusion[1]?>"></li>
 						<li class="c<?php echo $solusion[2]?>"></li>
@@ -35,6 +36,7 @@ class VueFin{
 							foreach($plateau[0] as $tab){
 								echo "<ul>";
 								$indice=1;
+								echo "<li id=indice>".$i."</li>";
 								foreach($tab as $val){
 									echo "<li id=\"".$indice."\" class=\"c".$val."\"></li>";
 									$indice++;
@@ -70,7 +72,7 @@ class VueFin{
 							<li class="c8" ></li>
 						</ul>
 					</div>
-	
+
 					<div id="bouton">
 						<form method="post" action="index.php" name="choix">
 							<input type="submit" id="rejouer" value="rejouer"/>
